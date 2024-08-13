aab9b556e939b8eb2dbfc5677fa79cb93a299535
BEBOLDER\jhon.sanchez <jhon.sanchez@bebolder.co>
2024-08-12 19:24:35 -0500

    Ajuste en Calculadora

---
+++ EjerciciosProgFuncionalBeBolder/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+O
+src/Punto1/MutableClassCar.java,b\5\b50d97306ea35346579a7fd717b26c83d726bdc2
--- EjerciciosProgFuncionalBeBolder/src/Punto8/Calculadora.java
+++ EjerciciosProgFuncionalBeBolder/src/Punto8/Calculadora.java
@@ -1,41 +1,58 @@
 package Punto8;
 
 import java.util.Scanner;
-import java.util.function.BiFunction;
 
 public class Calculadora {
+    private double total;
 
+    public Calculadora(double total){
+        this.total = total;
+        System.out.println("Valor Inicial " + total);
+    }
 
+    private Calculadora sumanding(double value){
+
+        System.out.println("Acumulado de " + total + " mas " + value);
+        total += value;
+        System.out.println("es de " + total );
+        return this;
+    }
+
+    private Calculadora restanding(double value){
+        System.out.println("Acumulado de " + total + " menos " + value);
+        total -= value;
+        System.out.println("es de " + total );
+        return this;
+    }
+
+    private Calculadora multiplicanding(double value){
+        System.out.println("Acumulado de " + total + " multiplicado por  " + value);
+        total *= value;
+        System.out.println("es de " + total );
+        return this;
+    }
+
+    public double getResult(){
+        return total;
+    }
 
     public static void main(String[] args) {
-        BiFunction<Integer, Integer, Integer> suma = (x, y) -> {
-            System.out.println("Sumando " + x + " por " + y);
-            return x + y;
-        };
-        BiFunction<Integer, Integer, Integer> resta = (x, y) -> {
-            System.out.println("restando " + x + " por " + y);
-            return x - y;
-        };
-        BiFunction<Integer, Integer, Integer> multiplicacion = (x, y) -> {
-            System.out.println("Multiplicando " + x + " por " + y);
-            return x * y;
-        };
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("ingrese un numero");
+        System.out.println("ingrese un numero inicial");
         int primerNum = scanner.nextInt();
-        System.out.println("ingrese por cuanto lo quiere multiplicar");
-        int segundoNum = scanner.nextInt();
-        System.out.println(multiplicacion.apply(primerNum, segundoNum));
         System.out.println("ingrese cuanto le quiere sumar");
-        int tercerNum = scanner.nextInt();
-        System.out.println(suma.apply(multiplicacion.apply(primerNum, segundoNum), tercerNum));
+        int segundoNum = scanner.nextInt();
         System.out.println("ingrese cuanto le quiere restar");
+        int tercerNum = scanner.nextInt();
+        System.out.println("ingrese por cuanto lo quiere multiplicar");
         int cuartoNum = scanner.nextInt();
-        System.out.println(resta.apply((suma.apply(multiplicacion.apply(primerNum, segundoNum), tercerNum)),cuartoNum));
 
-
+        Calculadora calculadora = new Calculadora(primerNum);
+        calculadora.sumanding(segundoNum)
+                .restanding(tercerNum)
+                .multiplicanding(cuartoNum);
+        System.out.println("El resultado es " + calculadora.getResult());
     }
 
 }
